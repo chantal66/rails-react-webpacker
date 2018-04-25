4f1753c0d697835a334c6d556c6c3f80713ad268
Chantal Justamond <cjustamm66@me.com>
2018-04-25 11:06:20 -0600

    able to add components to a rails app

---
+++ app/javascript/components/GreetUser.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class GreetUser extends React.Component {
+  render () {
+    return (
+        <div>
+            <h1>Hello {this.props.name}, </h1>
+            <h3>WOW! This is a component!</h3>
+        </div>
+        );
+  }
+}
+
+GreetUser.propTypes = {
+  name: PropTypes.string
+};
+export default GreetUser;
